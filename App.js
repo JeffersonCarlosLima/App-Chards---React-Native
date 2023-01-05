a65bcf2cc548b5321c67d6e545cbd55fdec0f2c9
Jefferson Carlos Lima <92160349+JeffersonCarlosLima@users.noreply.github.com>
2023-01-05 17:48:53 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/Interfaces/Home';
+import Login from './src/Interfaces/Login';
+
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
